a49cbac6ba0f4e8549175dd3abaa40c43ec17959
happyhaijuan <zhanghaijuan46@126.com>
2019-12-30 16:57:10 -0600

    add register

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
 	path('login/', LoginView.as_view(template_name='users/login.html'), name = 'login'),
 	# 注销
 	path('logout/', views.logout_view, name = 'logout'),
+	# 注册页面
+	path('register/', views.register, name = 'register'),
 ]
