466fe949e00d6b68bc82bf28047639447df9b585
haijuan <haijuan@iastate.edu>
2020-02-08 11:59:48 -0600

    Ready for deployment to heroku

--- settings.py
+++ settings.py
@@ -37,11 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #第三方应用程序
-    'bootstrap3',
     # My apps
     'learning_logs',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -123,25 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#django-bootstrap3设置
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
-# Heroku设置
-cwd = os.getcwd()
-if cwd == '/app' or cwd[:4] == '/tmp':
-    import dj_database_url
-    DATABASES = {
-            'default': dj_database_url.config(default='postgres://localhost')
-
-    }
-# 让request.is_secure()承认X-Forwarded-Proto头
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# 支持所有的主机头（host header）
-ALLOWED_HOSTS = ['*']
-# 静态资产配置
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),
-    )
-
